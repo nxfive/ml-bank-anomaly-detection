8f7091b0d5185f920b8b08d4f76db68c104c7489
nxfive <kontakt@nxfive.pl>
2025-09-08 21:35:34 +0200

    feat: Add dynamic config path

--- src/models/model_features.py
+++ src/models/model_features.py
@@ -1,4 +1,6 @@
 import yaml
+import os
+
 import pandas as pd
 
 
@@ -8,10 +10,11 @@ def get_model_features(
     """
     Remove not-useful features from train and test DataFrames based on config.yml.
     """
-    with open("config.yml", "r") as file:
+    config_path = os.path.join(os.path.dirname(__file__), "config.yml")
+    with open(config_path, "r") as file:
         params = yaml.safe_load(file)
 
     train_df.drop(columns=params["not_usefull_features"], inplace=True)
-    test_df.drop(column=params["not_usefull_features"], inplace=True)
+    test_df.drop(columns=params["not_usefull_features"], inplace=True)
 
     return train_df, test_df
