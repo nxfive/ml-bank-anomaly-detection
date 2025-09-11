b6dfcb7cef8646c2cfe22f834061062c2eedc3c9
nxfive <kontakt@nxfive.pl>
2025-09-11 15:34:32 +0200

    feat: Add paths config file with PROJECT_ROOT and directories

---
+++ src/utils/paths.py
@@ -0,0 +1,12 @@
+import os
+
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+
+CONFIG_DIR = os.path.join(PROJECT_ROOT, "config")
+LOGS_DIR = os.path.join(PROJECT_ROOT, "logs")
+MODELS_DIR = os.path.join(PROJECT_ROOT, "models")
+
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
