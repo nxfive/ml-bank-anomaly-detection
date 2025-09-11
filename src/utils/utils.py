d8450942e4f45f3a7d379ed9442ddcea08446749
nxfive <kontakt@nxfive.pl>
2025-09-11 15:27:36 +0200

    feat: Save models and add function to load metadata

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,32 @@
 import os
+import pickle
+from typing import Any
+
+import pandas as pd
+import yaml
+
+from .paths import MODELS_DIR, PROCESSED_DATA_DIR
 
 
-def save_data(**kwargs):
-    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-    processed_dir = os.path.join(PROJECT_ROOT, "data", "processed")
+def save_objects(**kwargs):
+    """
+    Save DataFrames as parquet fiels and objects as pickle files to disk.
+    """
+    for name, obj in kwargs.items():
+        if isinstance(obj, pd.DataFrame):
+            path = os.path.join(PROCESSED_DATA_DIR, f"{name}.parquet")
+            obj.to_parquet(path)
+        else:
+            path = os.path.join(MODELS_DIR, f"{name}.pkl")
+            with open(path, "wb") as f:
+                pickle.dump(obj, f)
+        print(f"Saved {name} -> {path}")
 
-    for name, data in kwargs.items():
-        data.to_parquet(os.path.join(processed_dir, f"{name}.parquet"))
+
+def load_model_metadata(model_name: str) -> dict[str, Any]:
+    """
+    Load and return metadata/configuration for the specified model.
+    """
+    meta_path = os.path.join(MODELS_DIR, "metadata", f"{model_name}.yml")
+    with open(meta_path, "r") as file:
+        return yaml.safe_load(file)
