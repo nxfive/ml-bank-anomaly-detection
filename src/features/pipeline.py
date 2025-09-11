5b2bb02a24af415fe2530a9e1677a425a636f8b8
nxfive <kontakt@nxfive.pl>
2025-09-11 14:59:47 +0200

    feat: Add optional data splitting logic to feature pipeline

--- src/features/pipeline.py
+++ src/features/pipeline.py
@@ -1,37 +1,40 @@
-import pandas as pd
 import time
-from .utils import (
-    add_rolling_features,
-    add_group_based_features,
-    add_unusual_usage_features,
-    add_time_since_last_transaction,
-)
+
+import pandas as pd
+from src.utils.logger import get_logger, setup_logging
+
 from .features import (
+    data_splitting,
     feature_extraction,
     feature_transformation,
     feature_without_data_leakage_risk,
-    data_splitting,
 )
-from src.utils.logger import setup_logging, get_logger
+from .utils import (
+    add_group_based_features,
+    add_rolling_features,
+    add_time_since_last_transaction,
+    add_unusual_usage_features,
+)
 
 setup_logging()
 logger = get_logger(name="features")
 
 
-def run_features_pipeline(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+def run_features_pipeline(
+    df: pd.DataFrame, split_data: bool = True
+) -> tuple[pd.DataFrame, pd.DataFrame] | pd.DataFrame:
     """
     Applies all feature engineering steps to the DataFrame with logging.
+    Optionally splits the data into training and testing sets.
     """
     logger.info("Starting feature pipeline...")
 
     start = time.time()
+    df = df.copy()
     df = feature_extraction(df)
     df = feature_transformation(df)
     df = feature_without_data_leakage_risk(df)
     logger.info(f"Basic feature engineering complete in {time.time() - start:.2f}s")
-
-    train_df, test_df = data_splitting(df)
-    logger.info(f"Data split into train ({len(train_df)}) and test ({len(test_df)})")
 
     feature_functions = [
         add_rolling_features,
@@ -40,12 +43,26 @@ def run_features_pipeline(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]
         add_time_since_last_transaction,
     ]
 
-    for func in feature_functions:
-        start = time.time()
-        train_df = func(train_df)
-        test_df = func(test_df)
-        logger.info(f"{func.__name__} applied in {time.time() - start:.2f}s")
+    if split_data:
+        train_df, test_df = data_splitting(df)
+        logger.info(
+            f"Data split into train ({len(train_df)}) and test ({len(test_df)})"
+        )
 
-    logger.info("All feature engineering steps completed.")
+        for func in feature_functions:
+            start = time.time()
+            train_df = func(train_df)
+            test_df = func(test_df)
+            logger.info(f"{func.__name__} applied in {time.time() - start:.2f}s")
 
-    return train_df, test_df
+        logger.info("All feature engineering steps completed.")
+        return train_df, test_df
+
+    else:
+        for func in feature_functions:
+            start = time.time()
+            df = func(df)
+            logger.info(f"{func.__name__} applied in {time.time() - start:.2f}s")
+
+        logger.info("All feature engineering steps completed.")
+        return df
