420cc3b04a83dfa9cc3aeba88431e312d5694e02
nxfive <kontakt@nxfive.pl>
2025-09-12 20:57:36 +0200

    feat: Drop not-useful columns from single dataframe

--- src/models/model_features.py
+++ src/models/model_features.py
@@ -5,16 +5,24 @@ import pandas as pd
 
 
 def get_model_features(
-    train_df: pd.DataFrame, test_df: pd.DataFrame
-) -> tuple[pd.DataFrame, pd.DataFrame]:
+    train_df: pd.DataFrame, test_df: pd.DataFrame | None = None
+) -> tuple[pd.DataFrame, pd.DataFrame] | pd.DataFrame:
     """
-    Remove not-useful features from train and test DataFrames based on config.yml.
+    Remove not-useful features from train/test DataFrames or from a single DataFrame
+    based on config.yml.
     """
     config_path = os.path.join(os.path.dirname(__file__), "config.yml")
     with open(config_path, "r") as file:
         params = yaml.safe_load(file)
 
-    train_df.drop(columns=params["not_usefull_features"], inplace=True)
-    test_df.drop(columns=params["not_usefull_features"], inplace=True)
+    drop_cols = params["not_usefull_features"]
 
-    return train_df, test_df
+    if test_df is not None:
+        train_df, test_df = train_df.copy(), test_df.copy()
+        return (
+            train_df.drop(columns=drop_cols, errors="ignore"),
+            test_df.drop(columns=drop_cols, errors="ignore"),
+        )
+    else:
+        train_df = train_df.copy()
+        return train_df.drop(columns=drop_cols, errors="ignore")
