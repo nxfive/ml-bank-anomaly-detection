bb92d9fbf6890fd2074e457aab21956386e8a455
nxfive <kontakt@nxfive.pl>
2025-09-22 16:37:25 +0200

    feat: Expose /metrics endpoint and test error routes

--- server/run.py
+++ server/run.py
@@ -1,12 +1,33 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from prometheus_client import make_asgi_app
 
-from server.app.routers import api
+from server.app.routers import api, metrics
 
+import os
 
 app = FastAPI()
 app.include_router(api.router)
 
 
+if os.getenv("ENV") == "dev":
+
+    @app.get("/error500")
+    @metrics.track_metrics
+    async def error500():
+        raise RuntimeError("Test 500 error")
+
+    @app.get("/error400")
+    @metrics.track_metrics
+    async def error400():
+        return Response(content="Bad Request", status_code=400)
+
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=8000, workers=1, reload=False, log_level="info")
+    uvicorn.run(
+        app=app, host="0.0.0.0", port=8000, workers=1, reload=False, log_level="info"
+    )
