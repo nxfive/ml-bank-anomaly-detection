a0b4006cbf84fba8312172bbdcae153d7424a5eb
nxfive <kontakt@nxfive.pl>
2025-09-25 19:41:12 +0200

    feat: Add elk config in logging setup

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,23 +4,32 @@ import os
 import yaml
 from datetime import datetime
 
-from .paths import CONFIG_DIR
+from .paths import CONFIG_DIR, ELK_CONFIG_DIR, LOGS_DIR, ELK_LOGS_DIR
 
 
 def setup_logging(default_level=logging.INFO):
-    config_path = os.path.join(CONFIG_DIR, "logger.yml")
+    env = os.getenv("ENV", "dev")
+
+    if env == "prod":
+        config_path = os.path.join(ELK_CONFIG_DIR, "elk_logger.yml")
+    else:
+        config_path = os.path.join(CONFIG_DIR, "logger.yml")
+
+    os.makedirs(LOGS_DIR, exist_ok=True)
+    os.makedirs(ELK_LOGS_DIR, exist_ok=True)
+
+    if env == "prod":
+        log_filename = os.path.join(ELK_LOGS_DIR, "app.log")
+    else:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        log_filename = os.path.join(LOGS_DIR, f"log_{timestamp}.log")
 
     if os.path.exists(config_path):
         with open(config_path, "r") as f:
             config = yaml.safe_load(f)
 
-        log_dir = os.path.join(os.path.dirname(config_path), "../logs")
-        os.makedirs(log_dir, exist_ok=True)
-
-        timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-        log_filename = os.path.join(log_dir, f"log_{timestamp}.log")
-
-        config["handlers"]["file"]["filename"] = log_filename
+        if "handlers" in config and "file" in config["handlers"]:
+            config["handlers"]["file"]["filename"] = log_filename
 
         logging.config.dictConfig(config)
     else:
