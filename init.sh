5483694d25aa26998f17fde3dd54730a847a89e3
nxfive <kontakt@nxfive.pl>
2025-09-25 23:01:43 +0200

    chore: Create user with password for filebeat in elasticsearch

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+until curl -s -k -u elastic:${ELASTIC_PASSWORD} ${ELASTIC_HOST}; do
+  echo "Waiting for Elasticsearch..."
+  sleep 5
+done
+
+curl -s -k -u elastic:${ELASTIC_PASSWORD} -X PUT "${ELASTIC_HOST}/_security/role/filebeat_writer_role" \
+--cacert /usr/share/elasticsearch/config/certs/ca.crt \
+-H 'Content-Type: application/json' -d '{
+  "cluster": ["manage_index_templates", "monitor", "read_ilm"],
+  "indices": [
+    {
+      "names": ["server-app-*"],
+      "privileges": ["write","create_index","manage"]
+    }
+  ]
+}'
+
+curl -s -k -u elastic:${ELASTIC_PASSWORD} -X POST "${ELASTIC_HOST}/_security/user/filebeat_writer" \
+--cacert /usr/share/elasticsearch/config/certs/ca.crt \
+-H "Content-Type: application/json" -d "{
+  \"password\": \"${ELASTIC_FILEBEAT_PASS}\",
+  \"roles\": [\"filebeat_writer_role\"],
+  \"full_name\": \"Filebeat Writer\"
+}"
+
+exec /usr/local/bin/docker-entrypoint.sh eswrapper
