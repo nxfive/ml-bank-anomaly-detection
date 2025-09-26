a0e150a42a371b20562bde4924dd606e42e41549
nxfive <kontakt@nxfive.pl>
2025-09-26 11:26:13 +0200

    chore: Add bash script to wait for elasticsearch

---
+++ kibana-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+until curl -s -k -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} https://elasticsearch:9200; do
+  echo "Waiting for Elasticsearch..."
+  sleep 5
+done
+
+exec /usr/share/kibana/bin/kibana-entrypoint
