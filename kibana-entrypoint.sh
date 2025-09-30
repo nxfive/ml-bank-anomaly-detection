f715d313a04d498b8101e62a67e642e64790e241
nxfive <kontakt@nxfive.pl>
2025-09-30 16:42:33 +0200

    chore: Change names of env variables for kibana

--- kibana-entrypoint.sh
+++ kibana-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-until curl -s -k -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} https://elasticsearch:9200 > /dev/null; do
+until curl -s -k -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} https://elasticsearch:9200 > /dev/null; do
   echo "Waiting for Elasticsearch..."
   sleep 5
 done
 
-KIBANA_TOKEN=$(curl -s -k -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} \
+KIBANA_TOKEN=$(curl -s -k -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} \
   -X POST "https://elasticsearch:9200/_security/service/kibana-system/credential/token" \
   -H "Content-Type: application/json" \
   | grep -oP '"value":"\K[^"]+')
