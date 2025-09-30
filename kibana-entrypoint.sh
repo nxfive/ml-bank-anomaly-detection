f8567675d8c097a05ecf27a82697a77c1b73fb3a
nxfive <kontakt@nxfive.pl>
2025-09-30 15:16:19 +0200

    chore: Generate token for kibana service

--- kibana-entrypoint.sh
+++ kibana-entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-until curl -s -k -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} https://elasticsearch:9200; do
+until curl -s -k -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} https://elasticsearch:9200 > /dev/null; do
   echo "Waiting for Elasticsearch..."
   sleep 5
 done
+
+KIBANA_TOKEN=$(curl -s -k -u elastic:${ELASTIC_PASSWORD} \
+  -X POST "https://elasticsearch:9200/_security/service/elastic/kibana/credential/token" \
+  -H "Content-Type: application/json" \
+  | grep -oP '"value":"\K[^"]+')
+
+export ELASTICSEARCH_USERNAME="elastic/kibana"
+export ELASTICSEARCH_PASSWORD=$KIBANA_TOKEN
 
 exec /usr/local/bin/kibana-docker "$@"
