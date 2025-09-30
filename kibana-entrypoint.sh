89edd6da224cdba906d9f0cdfe54e2fd51c8336d
nxfive <kontakt@nxfive.pl>
2025-09-30 15:38:28 +0200

    chore[fix]: Change kibana username to kibana-system

--- kibana-entrypoint.sh
+++ kibana-entrypoint.sh
@@ -4,12 +4,12 @@ until curl -s -k -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} https://
   sleep 5
 done
 
-KIBANA_TOKEN=$(curl -s -k -u elastic:${ELASTIC_PASSWORD} \
-  -X POST "https://elasticsearch:9200/_security/service/elastic/kibana/credential/token" \
+KIBANA_TOKEN=$(curl -s -k -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} \
+  -X POST "https://elasticsearch:9200/_security/service/kibana-system/credential/token" \
   -H "Content-Type: application/json" \
   | grep -oP '"value":"\K[^"]+')
 
-export ELASTICSEARCH_USERNAME="elastic/kibana"
+export ELASTICSEARCH_USERNAME="kibana-system"
 export ELASTICSEARCH_PASSWORD=$KIBANA_TOKEN
 
 exec /usr/local/bin/kibana-docker "$@"
