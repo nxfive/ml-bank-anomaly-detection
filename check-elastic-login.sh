edc3cab6bafbabad8fe0ebaa7fc1cf266e131bb4
nxfive <kontakt@nxfive.pl>
2025-10-15 22:09:29 +0200

    chore: Create script to check login to elastic for kibana/filebeat

---
+++ check-elastic-login.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+while ! curl -sf "${ELASTIC_HOST}/-/ready" > /dev/null; do
+  echo "Waiting for Elasticsearch..."
+  sleep 2
+done
+
+until curl -sf -u "${ELASTIC_USER}:${ELASTIC_PASS}" "${ELASTIC_HOST}" > /dev/null; do
+    echo "Unable to log in as user ${ELASTIC_USER}, waiting..."
+    sleep 5
+done
+echo "Login successful - starting ${SERVICE}..."
+
+if [ "$SERVICE" = "kibana" ]; then
+  exec /usr/local/bin/kibana-docker "$@"
+elif [ "$SERVICE" = "filebeat" ]; then
+  exec /usr/share/filebeat/filebeat -e -strict.perms=false
+else
+  echo "Unknown service. Exiting... "
+  exit 1
+fi  
