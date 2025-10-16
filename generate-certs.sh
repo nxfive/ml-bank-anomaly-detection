0dac8dba71996a95ba8ef6c9687d493c4f4d3a11
nxfive <kontakt@nxfive.pl>
2025-10-16 19:08:12 +0200

    chore[fix]: Generate separate CA and sign elastic certificate

--- generate-certs.sh
+++ generate-certs.sh
@@ -7,11 +7,15 @@ CERT_DIR="/home/ubuntu/${APP_DIR}/elk/certs"
 mkdir -p "$CERT_DIR"
 cd "$CERT_DIR"
 
+openssl genrsa -out ca.key 2048
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 -out ca.crt -subj "/CN=cn-ca"
+
 openssl genrsa -out elasticsearch.key 2048
 
-openssl req -new -x509 -key elasticsearch.key -out elasticsearch.crt -days 365 \
-  -subj "/CN=elasticsearch"
+openssl req -new -key elasticsearch.key -out elasticsearch.csr -subj "/CN=elasticsearch"
 
-cp elasticsearch.crt ca.crt
+openssl x509 -req -in elasticsearch.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out elasticsearch.crt -days 365 -sha256
+
+rm elasticsearch.csr
 
 echo "Certificates generated in $CERT_DIR"
