e6bd1592939440d588d3272ccd0913ef528efb94
nxfive <kontakt@nxfive.pl>
2025-10-21 18:45:59 +0200

    chore: Remove duplicated code and unset env variables

--- check-elastic-login.sh
+++ check-elastic-login.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
-while ! curl -sf -u "${ELASTIC_USER}:${ELASTIC_PASS}" "${ELASTIC_HOST}" --cacert "${ELASTIC_CACERT}" > /dev/null; do
-  echo "Waiting for Elasticsearch..."
-  sleep 2
+echo "Waiting for Elasticsearch..."
+until curl -v -u "${ELASTIC_USER}:${ELASTIC_PASS}" "${ELASTIC_HOST}" --cacert "${ELASTIC_CACERT}" > /dev/null; do
+  echo "Unable to log in as user ${ELASTIC_USER}, retrying in 5s..."
+  sleep 5
 done
 
-until curl -sf -u "${ELASTIC_USER}:${ELASTIC_PASS}" "${ELASTIC_HOST}" --cacert "${ELASTIC_CACERT}" > /dev/null; do
-    echo "Unable to log in as user ${ELASTIC_USER}, waiting..."
-    sleep 5
-done
 echo "Login successful - starting ${SERVICE}..."
+
+unset ELASTIC_USER ELASTIC_PASS ELASTIC_HOST ELASTIC_CACERT KIBANA_ENCRYPTION_KEY KIBANA_REPORTING_KEY KIBANA_SECURITY_KEY
 
 if [ "$SERVICE" = "kibana" ]; then
   exec /usr/local/bin/kibana-docker "$@"
 elif [ "$SERVICE" = "filebeat" ]; then
-  exec /usr/share/filebeat/filebeat -c /usr/share/filebeat/filebeat.yml -e -strict.perms=false
+  exec /usr/share/filebeat/filebeat -c /usr/share/filebeat/filebeat.yml -e
 else
-  echo "Unknown service. Exiting... "
+  echo "Unknown service. Exiting..."
   exit 1
-fi  
+fi
