269d80b2e4bc0c6d7c5ba3949cfb165335623b35
nxfive <kontakt@nxfive.pl>
2025-10-21 18:33:20 +0200

    chore: Add kibana template to create kibana.yml with env vars

--- config.sh
+++ config.sh
@@ -1,10 +1,13 @@
-#!bin/bash
+#!/bin/bash
 
 echo "Creating alertmanager.yml from template..."
 envsubst < /home/ubuntu/${APP_DIR}/alertmanager/alertmanager.yml.template > /home/ubuntu/${APP_DIR}/alertmanager/alertmanager.yml
 
 echo "Creating filebeat.yml from template..."
-envsubst < /home/ubuntu/${APP_DIR}/elk/filebeat.yml.template > /home/ubuntu/${APP_DIR}/elk/filebeat.yml
+envsubst < /home/ubuntu/${APP_DIR}/elk/filebeat/filebeat.yml.template > /home/ubuntu/${APP_DIR}/elk/filebeat/filebeat.yml
 
-sudo chown root:root /home/ubuntu/${APP_DIR}/elk/filebeat.yml
-sudo chmod 644 /home/ubuntu/${APP_DIR}/elk/filebeat.yml
+echo "Creating kibana.yml from template..."
+envsubst < /home/ubuntu/${APP_DIR}/elk/kibana/kibana.yml.template > /home/ubuntu/${APP_DIR}/elk/kibana/kibana.yml
+
+sudo chown root:root /home/ubuntu/${APP_DIR}/elk/filebeat/filebeat.yml
+sudo chmod 600 /home/ubuntu/${APP_DIR}/elk/filebeat/filebeat.yml
