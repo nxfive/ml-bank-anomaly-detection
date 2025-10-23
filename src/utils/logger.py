f14c9a5c038e1d47e870c8dfac9137d53b0c3486
nxfive <kontakt@nxfive.pl>
2025-10-23 16:47:01 +0200

    feat: Ensure file/console logging only in dev; ELK(console) logging in prod

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -10,17 +10,13 @@ from .paths import CONFIG_DIR, ELK_CONFIG_DIR, LOGS_DIR, ELK_LOGS_DIR
 def setup_logging(default_level=logging.INFO):
     env = os.getenv("ENV", "dev")
 
-    if env == "prod":
-        config_path = os.path.join(ELK_CONFIG_DIR, "elk_logger.yml")
-    else:
-        config_path = os.path.join(CONFIG_DIR, "logger.yml")
-
     os.makedirs(LOGS_DIR, exist_ok=True)
     os.makedirs(ELK_LOGS_DIR, exist_ok=True)
 
     if env == "prod":
-        log_filename = os.path.join(ELK_LOGS_DIR, "app.log")
+        config_path = os.path.join(ELK_CONFIG_DIR, "elk_logger.yml")
     else:
+        config_path = os.path.join(CONFIG_DIR, "logger.yml")
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         log_filename = os.path.join(LOGS_DIR, f"log_{timestamp}.log")
 
@@ -28,8 +24,9 @@ def setup_logging(default_level=logging.INFO):
         with open(config_path, "r") as f:
             config = yaml.safe_load(f)
 
-        if "handlers" in config and "file" in config["handlers"]:
-            config["handlers"]["file"]["filename"] = log_filename
+        file_handler = config.get("handlers", {}).get("file")
+        if file_handler is not None:
+            file_handler["filename"] = log_filename
 
         logging.config.dictConfig(config)
     else:
